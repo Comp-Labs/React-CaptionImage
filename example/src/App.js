a5b089f08b25821547039de732030354bf9dcc7e
Rudra Sen <rudra_sen@outlook.com>
2022-08-29 19:48:31 +0530

    Initial Commit

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@techfiddle/react-image-caption'
+import '@techfiddle/react-image-caption/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
